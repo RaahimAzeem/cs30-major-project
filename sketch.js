a41356f05fc2652c3a9a5126f1513991bd8fde5d
RaahimAzeem <raahimazeem08@gmail.com>
2024-05-09 14:04:19 -0600

    Initializing Start Screen.

--- sketch.js
+++ sketch.js
@@ -1,17 +1,44 @@
-// Project Title
-// Your Name
-// Date
+// CS30 Major Project (Sudoku)
+// Muhammad Raahim
+// June 14, 2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - 
 
+let state = "start screen";
+let grid;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  determineState();
+}
 
-  square(mouseX, mouseY, 100);
+function determineState() {
+  if (state === "start screen") {
+    startScreen();
+  }
+  else if (state === "game screen") {
+    gameScreen();
+  }
+}
+
+function gameScreen() {
+  background(255);
+}
+
+function startScreen() {
+  background(0);
+  square(width/2, height/2, 100);
+
+}
+
+function mousePressed() {
+  // Checking to see if the user has clicked on the square on the start screen
+  if (mouseX > width/2 && mouseX < width/2 + 100 && mouseY > height/2 && mouseY < height/2 + 100){
+    state = "game screen";
+  }
+  
 }
